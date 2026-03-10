7ca230bf90a6178bb062ee4bf3bf17be91bdc911
Labo_0.02 <iurbinar@uteq.edu.ec>
2026-03-10 13:27:47 -0500

    Diseño mejorado dela interfaces e implementacion de 2 actividades de inicio y resultados.

---
+++ app/src/main/java/com/example/espejo_ancestral/ResultadoActivity.java
@@ -0,0 +1,35 @@
+package com.example.espejo_ancestral;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoActivity extends AppCompatActivity {
+
+    TextView txtResultado;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        txtResultado = findViewById(R.id.txtResultado);
+
+        String resultado = getIntent().getStringExtra("resultado");
+
+        txtResultado.setText(resultado);
+    }
+    public void volverPantallaPrincipal(View view) {
+        finish();
+    }
+}
